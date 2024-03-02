0536dc1d47d62c3ebcd9884eeb2f491478c8e64f
Matt Maas <mattmaas7@gmail.com>
2024-03-02 09:50:55 -0600

    add Azure Dependency Injection

---
+++ CongressDataCollector/CongressDataCollector/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using Microsoft.Azure.Cosmos;
+using System;
+
+[assembly: FunctionsStartup(typeof(CongressDataCollector.Startup))]
+
+namespace CongressDataCollector
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Register HttpClient as a singleton
+            builder.Services.AddHttpClient();
+
+            // Register CosmosClient or any other services
+            builder.Services.AddSingleton((s) =>
+            {
+                var connectionString = Environment.GetEnvironmentVariable("CosmosDBConnection");
+                return new CosmosClient(connectionString);
+            });
+
+            // Add more services as needed
+        }
+    }
+}
