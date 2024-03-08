93cae5fda5643bebedf6267e36d6f6653a9ace2b
Matt Maas <mattmaas7@gmail.com>
2024-03-07 20:34:56 -0600

    Add openai chat gpt 4 document processing

--- CongressDataCollector/CongressDataCollector.Core/Interfaces/IBillService.cs
+++ CongressDataCollector/CongressDataCollector.Core/Interfaces/IBillService.cs
@@ -7,4 +7,6 @@ public interface IBillService
 {
     Task<BillsFetchResult> FetchBillsAsync(FetchState state, ILogger log, DateTime? fromDateTime = null, DateTime? toDateTime = null, string sort = "updateDate+desc");
     Task<Bill> FetchBillDetailsAsync(Bill bill, FetchState state, ILogger log);
+    Task FetchBillTextAsync(Bill bill, ILogger log, Func<Task> onSuccessCallback);
+
 }
---
+++ CongressDataCollector/CongressDataCollector.Core/Models/OpenAiSummaries.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace CongressDataCollector.Core.Models;
+
+public class OpenAiSummaries
+{
+    [JsonProperty("summary")]
+    public string Summary { get; set; }
+    [JsonProperty("keyPoints")]
+    public List<string> KeyPoints { get; set; }
+}
